48371281eb5e572d88b7099e608e79ad92c78e8a
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-11 11:37:28 +0530

    state is added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React,{Component} from "react";
 import 'tachyons';
 import CardList from "./CardList";
 import {robots} from './data/robots';
 import SearchBox from './SearchBox'
 
-const App=()=>{
+class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            robots: robots,
+            searchfield:''
+        }
+    }
+render(){
     return (
         <div class='tc'>
         <h1>RobotsFriends</h1>
         <SearchBox/>
-        <CardList robots={robots}/>
+        <CardList robots={this.state.robots}/>
         </div>
     );
+}
 }
 
 export default App; 
