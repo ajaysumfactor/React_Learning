e05f9a4daebe9f939a84905b0451fddac6c9ea82
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-12 15:54:50 +0530

    23/430 create 2 usestate hooks for ,robots,and searchfields

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App(){
     //     console.log("constructor");
     // }
 
+ const [robots,setRobots]=useState([]);
+ const [searchfield,setSearchfields]=useState("");
+
+
+
+
     // componentDidMount(){
     //     fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
     //         return response.json();
@@ -25,15 +31,25 @@ function App(){
     //     })
     //     console.log("component did mount ");
     // }
-    onSearchChange=(event)=>{
+    const onSearchChange = (event)=>{
         // console.log(event.target.value);
-        this.setState({searchfield: event.target.value})}
+        setSearchfields( event.target.value)}
     
 // render(){
 //     const {robots,searchfield} = this.state;
 //     const filteredRobots = robots.filter(robots=>{
 //         return robots.name.toLowerCase().includes(searchfield.toLowerCase())
 //     })
+const filteredRobots = robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+    
+
+
+
+
+
+
     console.log("render");
      return !robots.length ? <h1>Loading...</h1> :(<div className='tc'>
         <h1>RobotsFriends</h1>
